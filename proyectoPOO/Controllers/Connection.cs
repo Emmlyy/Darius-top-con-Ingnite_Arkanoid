5f11b78a03d7a14a0a9f464187970f307f28e39d
RauPro <00012119@uca.edu.sv>
2020-05-31 17:05:06 -0600

    Add: Patron MVC and Fix Connection Class

---
+++ proyectoPOO/Controllers/Connection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace proyectoPOO
+{
+    public class Connection
+    {
+        private static string herokuConnectionString = @"
+          Host=ec2-34-230-149-169.compute-1.amazonaws.com;
+          Port=5432;
+          Username=uyuljahpmkxlja;
+          Password=4921322ee3865f89021745ab38442443be0959390d1f2cbea47d753fbf38e54e;
+          Database=dr4v19hahf06q;
+          Pooling=true;
+          SSL Mode=Require;
+          TrustServerCertificate=True;
+        ";
+
+        public static DataTable Query(string query)
+        {
+            NpgsqlConnection db = new NpgsqlConnection(herokuConnectionString);
+            DataSet ds = new DataSet();
+            db.Open();
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(query,db);
+            da.Fill(ds);
+            db.Close();
+            return ds.Tables[0];
+        }
+
+        public static void noQuery(string _noQuery)
+        {
+            NpgsqlConnection db = new NpgsqlConnection(herokuConnectionString);
+            db.Open();
+            NpgsqlCommand noQuery = new NpgsqlCommand(_noQuery, db);
+            noQuery.ExecuteNonQuery();
+            db.Close();
+        }
+    }
+}
---
+++ proyectoPOO/Views/Jugar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace proyectoPOO
+{ 
+    public partial class Jugar : UserControl
+    {    
+        public Jugar()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text.Equals(""))
+            {
+                MessageBox.Show("Debe ingresar un usuario para poder jugar!");
+            }
+            else
+            {
+                try
+                {
+                    string sql = "SELECT \"UserID\",\"Points\" FROM public.\"User\" WHERE \"UserID\"='"+txtUsuario.Text+"';";
+                    var user = Connection.Query(sql);
+                    string name = user.Rows[0][0].ToString();
+                    string points = user.Rows[0][1].ToString();
+                    MessageBox.Show("Bienvenido: " +name +"\nTus puntos actuales son: "+points);
+                }
+                catch (Exception)
+                {
+                    string noSql = "INSERT INTO public.\"User\"(\"UserID\", \"Points\")VALUES ('"+txtUsuario.Text+"', 0);";
+                    Connection.noQuery(noSql);
+                    MessageBox.Show("Registrado\n"+"Nickname: "+txtUsuario.Text+"\nPuntos: 0");
+                }
+            }
+        }
+    }
+}
